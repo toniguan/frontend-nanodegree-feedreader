e194957257ae60aaee8a7de4ab15ea89a7c95ad4
Tong Guan <toniguan@gmail.com>
2018-08-29 10:07:26 -0700

    loadFeed1 is nested in loadFeed0's call back

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,17 +9,10 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* Test suite: the RSS feeds definitions  */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
+        /* TC1: a test that ensures the allFeeds variable
+         * has been defined and that it is not empty.
          */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
@@ -27,48 +20,42 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* TC2: a test that ensures each feed in the allFeeds object
+         * has a URL defined and that the URL is not empty.
          */
          it('URL defined', function(){
            for(let feed of allFeeds){
              expect(feed.url).toBeDefined();
-             expect(feed.url.length).not.toBe(0);
+             expect(feed.url.length).toBeGreaterThan(0);
            }
 
          });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* TC3: a test that ensureseach feed in the allFeeds object
+         * has a name defined and that the name is not empty.
          */
          it('name defined', function(){
            for(let feed of allFeeds){
              expect(feed.name).toBeDefined();
-             expect(feed.name.length).not.toBe(0);
+             expect(feed.name.length).toBeGreaterThan(0);
            }
          })
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Test suite: The menu */
     describe('The menu', function(){
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* TC1: a test that ensures the menu element is
+         * hidden by default.
          */
          it('hidden by default', function(){
            expect($('body').hasClass('menu-hidden')).toBe(true);
          })
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* TC2: a test that ensures the menu changes visibility
+          * when the menu icon is clicked.
           */
           it('toggle display when clicked', function(){
 
@@ -84,14 +71,12 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test suite: Initial Entries */
     describe('Initial Entries',function(){
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* TC1: a test that ensures when the loadFeed function
+         * is called and completed, there is at least a single .entry
+         * element within the .feed container.
          */
 
          beforeEach(function(done) {
@@ -99,36 +84,36 @@ $(function() {
          });
 
          it('one entry is loaded', function(){
-           const container = document.querySelector('.feed');
-           expect(container.children.length > 0).toBe(true);
+           expect(document.querySelector('.feed').querySelectorAll('.entry').length).toBeGreaterThan(0);
          })
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Test suite: New Feed Selection */
     describe('New Feed Selection', function(){
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+        /* TC1: a test that ensures when a new feed is loaded
+         * by the loadFeed function that the feed content actually changes.
          */
          const container = document.querySelector('.feed');
-         let feed1 = [], feed2 = [];
-         let feed;
+         let feed0 = [], feed1 = [];
 
          beforeEach(function(done) {
+           let feed;
            loadFeed(0, function(){
              feed = container.children;
              for(let i = 0; i < feed.length; i++){
-               feed1.push(feed[i].innerText);
+               feed0.push(feed[i].innerText);
              }
+             // nested loadFeed1 in loadFeed0's call back
+             loadFeed(1, function(){
+               feed = container.children;
+               for(let i = 0; i<feed.length; i++){
+                 feed1.push(feed[i].innerText);
+               }
+               done();
+             });
            });
-           loadFeed(1, function(){
-             feed = container.children;
-             for(let i = 0; i < feed.length; i++){
-               feed2.push(feed[i].innerText);
-             }
-             done();
-           });
+
          });
 
          function isSameFeed(feed1, feed2){
@@ -139,7 +124,7 @@ $(function() {
            return true;
          }
          it('content changes', function(){
-           expect(isSameFeed(feed1, feed2)).toBe(false);
+           expect(isSameFeed(feed0, feed1)).toBe(false);
          })
     })
 }());
